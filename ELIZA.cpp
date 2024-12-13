aec4bebaeb533d06cc0f76b73928b36ceb7c1011
claraakhlaghi07 <akhlaghiclara@gmail.com>
2024-12-13 13:07:11 +0800

    Add files via upload

---
+++ ELIZA.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ELIZA.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ELIZA, "ELIZA" );
